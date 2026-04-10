ffba0ba740b600ee71c99a9ce9da7eb0240acc1b
Abinayapriya12 <priyakabinaya@gmail.com>
2026-04-10 19:22:07 +0530

    exercise

---
+++ UseMemo/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState ,useEffect,useMemo, useCallback} from 'react'
+import List from './list'
+
+function App() {
+   const [number, setNumber] = useState(0)
+    const[dark,setDark]=useState(false)
+
+    const getItems=useCallback(()=>{
+      return[
+        number +1,
+        number +2,
+        number +3]
+    },[number])
+  
+
+    // const getItems=useCallback(()=>{
+    //return[number+1,number+2,number+3]
+    //},[number])
+  
+    const theme={
+      backgroundColor:dark? "black":"white",
+      color: dark? "white":"black",
+    }
+
+  return (
+       <div style={theme}>
+ <input type="Number"
+ value={number}
+ onChange={(e)=>setNumber(parseInt(e.target.value))}/><br/>
+    <button onClick={()=>setDark((curr)=>!curr)}>Toggle button</button>
+    <List getItems={getItems}/>
+     </div>
+  )
+}
+
+export default App
---
+++ UseMemo/src/list.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+   const List =({getItems})=>{
+    const [items,setItems]=useState([])
+    useEffect(()=>{
+        setItems(getItems())
+        console.log("setting items")
+    },[getItems])
+    return (
+    <div>
+      {items.map((item,index)=>{
+       return <p key={index}>{item}</p>
+      })}
+    </div>
+  )
+}
+
+export default List
